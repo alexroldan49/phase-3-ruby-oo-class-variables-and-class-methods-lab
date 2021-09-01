e1fe49c0b28774885f50a978273555024c921504
Alexander Roldan <alexroldan49@gmail.com>
2021-09-01 11:53:44 -0400

    commit tree f1bdb4783cd8ef419a8dcea728da44f7ff9bcd28

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require 'pry'
+class Song
+
+    @@count = 0
+
+    @@genres = []
+
+    @@artist = []
+    
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artist << artist
+    end
+    
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+
+    def genre
+        @genre
+    end
+
+    def self.count 
+        @@count
+    end 
+
+    def self.genre
+
+    end
+
+    binding.pry
+end 
